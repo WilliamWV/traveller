290b88911a09c813b2188d50da86dd20b76e9374
William <william.w.vargas@gmail.com>
2017-03-05 12:12:33 -0300

    added definitions about player and bullet

--- traveller.js
+++ traveller.js
@@ -6,7 +6,9 @@ var sketchProc = function(processingInstance) {
     size(XDIMENTION, YDIMENTION);
     frameRate(FPS);
     var SPEED = 2;
-
+    var ATRITTION = 1;
+    var BULLETDELAY = FPS/5;
+    var BULLETSPEED = 5;
     //Possible states within the game
     var DEAD = 0;
     var ALIVE = 1;
@@ -21,7 +23,47 @@ var sketchProc = function(processingInstance) {
     var BUTTONWIDTH = XDIMENTION/4;
     var BUTTONHEIGHT = YDIMENTION/8;
 
-    
+    var PLAYERIMAGE = loadImage("../img/Traveller/Player.png");
+    var BULLETIMAGE = loadImage("../img/Traveller/Bullet.png");
+    var CANNONIMAGE = loadImage("../img/Traveller/Cannon.png");
+
+    var bullets = [];
+    var input =[];
+
+    var Bullet = function(x, y, xSpeed, ySpeed){
+        this.x = x;
+        this.y = y;
+         
+    };
+
+    var Player = function(x, y){
+        this.x = x;
+        this.y = y;
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        this.img = PLAYERIMAGE;
+        this.readyToShoot = true;
+        this.delay = 0;
+    };
+    Player.prototype.draw = function(){
+        image(this.img, this.x, this.y);
+        if(!this.readyToShoot){
+            this.delay -= 1;
+            if(this.delay <=0){
+              this.readyToShoot = true;
+            }
+        }
+
+    };
+    PLayer.prototype.shoot = function(){
+        if(this.readyToShoot){
+            this.readyToShoot = false;
+            this.delay = BULLETDELAY;
+            var bullet = new Bullet(this.x, this.y);
+            bullets[bullets.length] = bullet;
+        }
+
+    };
 
 
 
